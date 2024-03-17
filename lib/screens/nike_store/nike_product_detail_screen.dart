65095fba2323eda64cfae69b864bc18d9c33c222
Phils Garcia <eivanphils@gmail.com>
2024-03-16 22:41:23 -0300

    Agregada nuevas secciones a usar

---
+++ lib/screens/nike_store/nike_product_detail_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NikeProductDetailScreen extends StatelessWidget {
+   
+  const NikeProductDetailScreen({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+         child: Text('NikeProductDetailScreen'),
+      ),
+    );
+  }
+}
