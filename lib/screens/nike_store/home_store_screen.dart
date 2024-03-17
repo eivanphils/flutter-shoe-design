356b5ec56b224416503c9f0fff3c98551b66a566
Phils Garcia <eivanphils@gmail.com>
2024-03-17 00:27:05 -0300

    Se agregando estilos del home

--- lib/screens/nike_store/home_store_screen.dart
+++ lib/screens/nike_store/home_store_screen.dart
@@ -38,6 +38,7 @@ class _Body extends StatelessWidget {
     final size = MediaQuery.of(context).size;
     return SafeArea(
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           SizedBox(
             height: size.height * 0.02,
@@ -82,11 +83,126 @@ class _Background extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: double.infinity,
-      // color: Colors.purple[100],
-      child: child,
+    final size = MediaQuery.of(context).size;
+    return Stack(
+      children: [
+        SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: CustomPaint(
+            painter: _PaintLines(),
+          ),
+        ),
+        child,
+
+// first line
+        Positioned(
+            left: 0,
+            top: size.height * 0.5,
+            child: Container(
+              width: 5,
+              height: 5,
+              color: Colors.red,
+            )),
+
+        Positioned(
+            right: 0,
+            top: size.height * 0.40,
+            child: Container(
+              width: 5,
+              height: 5,
+              color: Colors.blue,
+            )),
+
+        Positioned(
+            left: 0,
+            top: size.height * 0.62,
+            child: Container(
+              width: 5,
+              height: 5,
+              color: Colors.green,
+            )),
+
+        Positioned(
+            right: 0,
+            top: size.height * 0.50,
+            child: Container(
+              width: 5,
+              height: 5,
+              color: Colors.blue,
+            )),
+      ],
     );
+  }
+}
+
+class _PaintLines extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final firstLine = Paint()
+      ..color = const Color(0xFF3A2EFE)
+      ..style = PaintingStyle.fill;
+
+    final firstPath = Path();
+    firstPath.moveTo(0, size.height * 0.4);
+    firstPath.lineTo(size.width, size.height * 0.3);
+    firstPath.lineTo(size.width, size.height * 0.4);
+    firstPath.lineTo(0, size.height * 0.5);
+    firstPath.lineTo(0, size.height * 0.4);
+    canvas.drawPath(firstPath, firstLine);
+
+    final secondLine = Paint()
+      ..color = const Color(0xFFEE7964)
+      ..style = PaintingStyle.fill;
+
+    final secondPath = Path();
+    secondPath.moveTo(0, size.height * 0.5);
+    secondPath.lineTo(size.width, size.height * 0.4);
+    secondPath.lineTo(size.width, size.height * 0.5);
+    secondPath.lineTo(0, size.height * 0.6);
+    secondPath.lineTo(0, size.height * 0.5);
+    canvas.drawPath(secondPath, secondLine);
+
+
+    final thirdLine = Paint()
+      ..color = const Color(0xFF3ABBBF)
+      ..style = PaintingStyle.fill;
+
+    final thirdPath = Path();
+    thirdPath.moveTo(0, size.height * 0.6);
+    thirdPath.lineTo(size.width, size.height * 0.5);
+    thirdPath.lineTo(size.width, size.height * 0.6);
+    thirdPath.lineTo(0, size.height * 0.7);
+    thirdPath.lineTo(0, size.height * 0.6);
+    canvas.drawPath(thirdPath, thirdLine);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    throw true;
+  }
+}
+
+class _DiagonalHeaderPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+
+    // paint properties
+    paint.color = const Color(0xff615aab);
+    paint.style = PaintingStyle.fill;
+
+    final path = Path();
+    path.lineTo(0, size.height * 0.3);
+    path.lineTo(size.width, size.height * 0.26);
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
